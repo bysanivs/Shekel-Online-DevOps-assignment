2e5cc16ff4f8c0f878ca9865abb245334c6f823e
bysanivs <bysanivs@gmail.com>
2018-12-09 15:15:35 -0500

    updated readme andadd script to run always for a service

--- readme.bash
+++ readme.bash
@@ -2,3 +2,9 @@
 #Script to check service status
 ./checkService.sh SERVICENAME TASK
 TASK=start|stop|status|restart
+
+#runservice is a script, we can use to check for a service and start it if it is not running and to make it run always we can add it to crontab as below format
+
+#crontab -e
+* * * * * /usr/scripts/runservice.sh servicename >> /usr/logs/runservice.log
+
